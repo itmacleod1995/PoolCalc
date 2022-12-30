cb166b584b2bf8e7a73924c5b75d79434848a27b
Ian <imacleod@mail.umw.edu>
2022-12-30 15:54:23 -0500

    Finished bicarb

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function calc() {
     var chem = document.getElementById("chemNamewel").value;
     /**
-     * Calculate Bicarbonate level
+     * Calculate Bicarbonate level with the assumption that you want it at 100
      */
     if (chem == "bicarb" || chem == "sodium bicarbonate") {
-        console.log("Bicarb!");
+        var change = document.getElementById("desiredChange").value;
+        var result = document.getElementById("res");
+
+        var x = (1.4 * 22 * (change / 10)).toFixed(2);
+        result.textContent = x + "lbs of bicarb";
     }
 }
