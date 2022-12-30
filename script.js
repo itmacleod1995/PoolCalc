ff4d944b524d8709f69fa29cad6b31cbcca88507
Ian <imacleod@mail.umw.edu>
2022-12-30 15:28:43 -0500

    started working on javscript

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc() {
+    var chem = document.getElementById("chemNamewel").value;
+    /**
+     * Calculate Bicarbonate level
+     */
+    if (chem == "bicarb" || chem == "sodium bicarbonate") {
+        console.log("Bicarb!");
+    }
+}
